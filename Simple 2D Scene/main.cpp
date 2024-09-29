0fbdb516d63f8d5f5f3e7aa0ddce3f9184f0d446
Jeffrey Lim <limjeffrey3@gmail.com>
2024-09-28 20:49:56 -0400

    Project 1: Simple 2D Scene

--- Simple 2D Scene/main.cpp
+++ Simple 2D Scene/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Jeffrey Lim
 * Assignment: Simple 2D Scene
-* Date due: 2023-09-20, 11:59pm
+* Date due: 2024-09-28, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
